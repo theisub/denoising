e0bfe60daa898c669c4a440e5fa45d0be601735e
unknown <theisub1@gmail.com>
2022-08-04 12:34:03 +0300

    Refactored to task requirements

--- task1/task1.py
+++ task1/task1.py
@@ -1,23 +1,27 @@
+def multiplicate(A):
+    multiply = 1
+    multiply_non_zero = 1
+    counter_zero = 0
+    for items in A:
+        multiply = multiply * items
+        if items == 0:
+            counter_zero += 1
+        else:
+            multiply_non_zero *= items
+            
+    output = [0] * len(A)
+    if multiply != 0:
+        for i in range(len(A)):
+            output[i] = multiply // A[i]
+    elif counter_zero == 1:
+        for i in range(len(A)):
+            if A[i] == 0:
+                output[i] = multiply_non_zero
+    return output
+
+
 
 input = [1,2,3,4]
-multiply = 1
-multiply_non_zero = 1
-counter_zero = 0
-for items in input:
-    multiply = multiply * items
-    if items == 0:
-        counter_zero += 1
-    else:
-        multiply_non_zero *= items
-        
-output = [0] * len(input)
-if multiply != 0:
-    for i in range(len(input)):
-        output[i] = multiply // input[i]
-elif counter_zero == 1:
-    for i in range(len(input)):
-        if input[i] == 0:
-            output[i] = multiply_non_zero
-
+output = multiplicate(input)
 print(output)
             
